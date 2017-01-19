545021b6b6d76218bd191823683039819a4baa5e
Andrés Sarmiento <Sarmiento@amatea.org>
2017-01-19 12:17:01 -0500

    instalando pug

--- server.js
+++ server.js
@@ -39,7 +39,8 @@ var app = express();
 
 app.set('port', process.env.PORT || 3000);
 app.use(morgan('dev'));
-
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 app.use(cookieParser());
 app.use(bodyParser.json() );   
 app.use(bodyParser.urlencoded({ 
