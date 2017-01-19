cab738f5e4b54765568a599abb62601f2694b1e1
Andrés Sarmiento <Sarmiento@amatea.org>
2017-01-19 17:51:25 -0500

    listo el codigo

--- controllers/home.js
+++ controllers/home.js
@@ -1,4 +1,5 @@
 const nodemailer = require('nodemailer');
+const async = require('async');
 const User = require('../dbconection/bosquesconection');
 
 exports.index = (req, res) => {
@@ -7,14 +8,46 @@ exports.index = (req, res) => {
   });
 };
 
-exports.inscripcion = (req, res) => {
+exports.inscripcion = (req, res, next) => {
   
-  var user = new User(req.body);
+  async.waterfall([
+    function(done){ 
+      const user = new User(req.body);
 
-  user.save(function(err) {
-    if (err) {
-      return res.send(err);
-    }
-    res.redirect('/');
+      user.save(function(err) {
+      if (err) {
+        return res.send(err);
+      }
+      user.save((err) => {
+            if (err) { return next(err); }
+            req.logIn(user, (err) => {
+              done(err, user);
+            });
+            res.redirect('/');
+          });
   });
-};
+},
+    function sendResetPasswordEmail(user, done) {
+      const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'sarmiento@amatea.org',
+          pass: 'cibsolar1609'
+        }
+      });
+      const mailOptions = {
+        to: user.correo,
+        from: 'amatea@amatea.org',
+        subject: 'Bosques para Volar',
+        html: '<h2>' + 'Hola '+ user.nombre + '</h2>'+ `Gracias por inscribirte.\n` + 'En breve estaremos enviandote más información.\n\n'
+              + '<h3> Gracias por contribuir a la armonía del Ser con la Naturaleza </h3>'
+      };
+      transporter.sendMail(mailOptions, function(err){
+        done(err);
+      });
+    }
+  ], (err) => {
+    if (err) { return next(err); }
+  });
+
+}
