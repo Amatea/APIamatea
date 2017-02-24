74264fb7cc80c7a400b4022def20ca108e290915
Andrés Sarmiento <Sarmiento@amatea.org>
2017-02-24 16:55:54 -0500

    eventos refactor

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ const mailService = require('./services/nodemail');
  * Connect to MongoDB.
  */
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URILO);
 mongoose.connection.on('error', () => {
   console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
   process.exit();
